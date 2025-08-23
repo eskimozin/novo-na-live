0f2779976f81eeae85f4b449e077adf81dca5589
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-08-23 16:35:25 -0300

    feat: organizando projeto e estilizando

---
+++ src/data/resources/resources.js
@@ -0,0 +1,5 @@
+export default {
+  "eskimozin-img": "https://eskimozin.github.io/links/img/favicon.png",
+  "github-repo": "https://github.com/eskimozin/novo-na-live",
+  "add-content-suggestion": "https://github.com/eskimozin/novo-na-live/issues/new",
+}
